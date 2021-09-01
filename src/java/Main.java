35568a39e3a5d2488b4adbda7b931d4b371629ad
Varga György <vargagyurivip@gmail.com>
2021-09-01 19:12:52 +0200

    create int[]

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,6 +1,22 @@
+import java.util.Random;
+
 public class Main {
     
     public static void main(String[] args) {
 
+        int[] randomNums = getRandomNums();
+
+
     }
+
+    public static int[] getRandomNums(){
+        int[] randomNums = new int[20];
+        int low = 10;
+        int high = 100;
+        Random ran= new Random();
+        for (int i = 0; i < randomNums.length; i++) {
+            randomNums[i] = ran.nextInt(high-low) + low;
+        }
+        return randomNums;
+    };
 }
